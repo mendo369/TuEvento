9310dbb817d552d7c06d5a3495ed69b97681ebc4
Luis David Mendoza <luis23luis33@gmail.com>
2021-11-26 15:54:47 -0500

    login_0.1

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Form } from '@angular/forms';
+import { Router } from '@angular/router';
 import { loginM } from '../models/login';
 import { LoginService } from '../services/login.service';
 
@@ -11,17 +12,15 @@ import { LoginService } from '../services/login.service';
 })
 export class LoginComponent{
 
-  constructor(private services: LoginService) {
-
-
+  constructor(private services: LoginService, private router:Router) {
    }
-
 
   loginM(loginP:Form){
     // console.log(loginP);
     // console.log(this.login);
     //subscribe devuelve una respuesta
     this.services.postSendLogin(this.login).subscribe(res=>{ console.log(res)});
+    this.router.navigate(['login']);
   }
 
   login: loginM= new loginM();
